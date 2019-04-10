e9a734392bfae8213ef96210e0c081db1ccecfde
Tom Duverdier <tomduverdier2000@gmail.com>
2019-04-10 16:20:56 +0200

    ye

---
+++ src/components/Board.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <li>
+      <ul v-for="item in list" :key="item.id">
+        <div>
+          <span class="image">Image: {{item.image}}</span><br />
+          <span class="name">Name: {{item.name}}</span><br />
+          <span class="isActive">actif ?:{{item.isActive}}</span>
+        </div>
+      </ul>
+    </li>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'board',
+  components: { },
+  props: {
+    list: {
+      type: Object
+    }
+  }
+
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,26 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Board :list="list"></Board>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Board from '@/components/Board.vue'
 
 export default {
   name: 'home',
-  components: {
-    HelloWorld
+  components: { Board },
+  data () {
+    return {
+      list: [
+        {
+          image: 'scr super image',
+          name: 'premier jeu',
+          isActive: true
+        }
+      ]
+    }
   }
 }
 </script>
