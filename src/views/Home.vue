ee263e88e51656de823f5d8803f5cabf71da02a3
abderaouf <a.serour@elno.fr>
2019-04-10 16:45:40 +0200

    back initialisé

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,22 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <div class="home">{{result}}</div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { getGames } from '@/services/GamesClient.js'
 
 export default {
   name: 'home',
-  components: {
-    HelloWorld
+  components: {},
+  data () {
+    return {
+      result: 0
+    }
+  },
+
+  async mounted () {
+    this.result = await getGames()
   }
 }
 </script>
