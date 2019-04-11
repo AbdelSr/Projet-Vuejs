a174b609579e550f56da96da4248974af1059656
abderaouf <a.serour@elno.fr>
2019-04-11 10:19:11 +0200

    Fichier db.json mis à jour

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -3,8 +3,8 @@
     <ul v-for="item in list" :key="item.id">
       <div class="game">
         <span class="image">{{item.image}}</span>
-        <span class="name">Nom: {{item.name}}</span>
-        <span class="isActive">DISPONIBLE{{item.isActive}}</span>
+        <span class="name">Nom : {{item.name}}</span>
+        <span class="isActive">DISPONIBLE :{{item.isActive}}</span>
       </div>
     </ul>
   </div>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -13,7 +13,7 @@ export default {
 
   data () {
     return {
-      result: 0
+      result: []
     }
   },
 
